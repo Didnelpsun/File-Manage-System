446863fe29332226fb50a02bf498dd8da0b5d5ad
Didnelpsun <2675350965@qq.com>
2022-01-23 23:29:27 +0800

    Update AppTest.java

--- src/test/java/org/didnelpsun/AppTest.java
+++ src/test/java/org/didnelpsun/AppTest.java
@@ -24,28 +24,28 @@ public class AppTest
     @Test
     public void shouldAnswerWithTrue()
     {
-        //1.读取配置文件
+        // 1.读取配置文件
         InputStream in = null;
         try {
              in = Resources.getResourceAsStream("SqlMapConfig.xml");
         } catch (IOException e) {
             e.printStackTrace();
         }
-        //2.创建SqlSessionFactory工厂
+        // 2.创建SqlSessionFactory工厂
         SqlSessionFactoryBuilder builder = new SqlSessionFactoryBuilder();
-        //SqlSessionFactory不能new
+        // SqlSessionFactory不能new
         SqlSessionFactory factory = builder.build(in);
-        //3.使用工厂生产SqlSession对象
+        // 3.使用工厂生产SqlSession对象
         SqlSession session = factory.openSession();
-        //4.使用SqlSession创建DAO接口的代理对象
+        // 4.使用SqlSession创建DAO接口的代理对象
         UserDAO userDAO = session.getMapper(UserDAO.class);
-        //5.使用代理对象执行方法
+        // 5.使用代理对象执行方法
         List<User> users = userDAO.FindAllUsers();
         for(User user : users){
-//            System.out.println(user);
+//           System.out.println(user);
             System.out.println(user.toString());
         }
-        //6.释放资源
+        // 6.释放资源
         session.close();
         try {
             assert in != null;
