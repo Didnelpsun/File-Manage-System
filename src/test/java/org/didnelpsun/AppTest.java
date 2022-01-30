5542cb4551a4f26239d88af1a85b81f49f68c380
Didnelpsun <2675350965@qq.com>
2022-01-30 23:53:24 +0800

    更新

--- src/test/java/org/didnelpsun/AppTest.java
+++ src/test/java/org/didnelpsun/AppTest.java
@@ -3,15 +3,13 @@ package org.didnelpsun;
 //import static org.junit.Assert.assertTrue;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.Calendar;
-import java.util.Date;
 import java.util.List;
 
 import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.didnelpsun.dao.UserDAO;
-import org.didnelpsun.dao.implement.UserDAOImplement;
 import org.didnelpsun.entity.User;
 import org.junit.After;
 import org.junit.Before;
@@ -19,6 +17,7 @@ import org.junit.Test;
 
 public class AppTest {
     private InputStream in;
+    private SqlSession session;
     private UserDAO userDAO;
 
     // 测试之前执行
@@ -34,31 +33,33 @@ public class AppTest {
         // 2.创建SqlSessionFactory工厂
         // SqlSessionFactory不能new
         SqlSessionFactory factory = new SqlSessionFactoryBuilder().build(in);
-        // 3.使用工厂对象创建DAO对象
-        userDAO = new UserDAOImplement(factory);
+        // 3.使用工厂生产SqlSession对象
+        session = factory.openSession();
+        // 4.使用SqlSession创建DAO接口的代理对象
+        userDAO = session.getMapper(UserDAO.class);
     }
 
     // 测试之后执行
     @After
     public void destroy() throws Exception {
+        // 提交事务，否则事务会回滚
+        session.commit();
+        // 6.释放资源
+        session.close();
         assert in != null;
         in.close();
     }
 
     @Test
     public void Test() {
-
-        // 5.使用自定义对象执行方法
-        // testSelectAllUsers();
+        // 5.使用代理对象执行方法
         // testInsertUser();
-        // testUpdateUser();
         // testSelectAllUsers();
         // testDeleteUser();
         // System.out.println("更新后：");
         // testSelectAllUsers();
-        // testSelectUser();
         // testSelectUsersByName();
-        testGetUsersSum();
+        testSelectUsersByCondition();
     }
 
     // 测试查询所有用户
@@ -67,31 +68,6 @@ public class AppTest {
         for (User user : users) {
             System.out.println(user.toString());
         }
-    }
-
-    // 测试插入用户
-    public void testInsertUser() {
-        Calendar c = Calendar.getInstance();
-        c.set(2000, 2, 14);
-        Date d = new Date();
-        d = c.getTime();
-        User user = new User("蓝新煜", "男", d, "福建省福州市");
-        userDAO.insertUser(user);
-    }
-
-    // 测试更新用户
-    public void testUpdateUser() {
-        Calendar c = Calendar.getInstance();
-        c.set(1998, 8, 21);
-        Date d = new Date();
-        d = c.getTime();
-        User user = new User(2, "黄康", "男", d, "湖北省鄂州市");
-        userDAO.updateUser(user);
-    }
-
-    // 测试删除用户
-    public void testDeleteUser() {
-        userDAO.deleteUser(3);
     }
 
     // 测试查询用户
@@ -108,8 +84,14 @@ public class AppTest {
         }
     }
 
-    // 测试获取用户总数
-    public void testGetUsersSum() {
-        System.out.println("用户总数为：" + userDAO.getUsersSum());
+    // 测试条件查询
+    public void testSelectUsersByCondition() {
+        User u = new User();
+        u.setId(1);
+        u.setSex("男");
+        List<User> users = userDAO.selectUsersByCondition(u);
+        for (User user : users) {
+            System.out.println(user.toString());
+        }
     }
 }
